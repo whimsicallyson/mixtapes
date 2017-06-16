4fdc8f6d9ef6eaef9f76ea066d7abeb15848f051
Allyson Lubimir <allyson.lubimir@gmail.com>
2017-06-15 22:18:26 -0400

    updates for 2017

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,7 +5,7 @@ require 'string_similarity'
 require 'song_db'
 
 class Song < ActiveRecord::Base
-  ALBUM_ARTIST = "Fog Creek & Trello Mixes"
+  ALBUM_ARTIST = "Fog Creek Mixes"
 
   belongs_to :mixtape, :touch => true
   has_many :likes
